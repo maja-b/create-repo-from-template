0caf22a3cf4c73be732ec2fa807b37152f49b1ac
maja-b <101280519+maja-b@users.noreply.github.com>
2022-11-14 15:12:30 +0100

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -5,12 +5,7 @@ for input_file in `tree -I "${ignore_files}" -Ffai --noreport`
 do
   if [ ! -d "${input_file}" ]; then
     echo "Processing file: ${input_file}"
-    gomplate 
-         --file "${input_file}"
-         --out "${input_file}"
-         --left-delim "<<["
-         --right-delim "]>>"
-         -c var=./github/scripts/customize.json
+    gomplate --file "${input_file}" --out "${input_file}" --left-delim "<<[" --right-delim "]>>" -c var=./github/scripts/customize.json
   fi
 done
 
