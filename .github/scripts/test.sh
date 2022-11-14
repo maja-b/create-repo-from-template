06d98579d41f24a0cc3dc7529780b741147fb53b
maja-b <101280519+maja-b@users.noreply.github.com>
2022-11-14 15:38:36 +0100

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,10 +1,1 @@
-#!/usr/bin/env bash
-ignore_files=".git|.gitignore|customize.json|README.md"
-
-for input_file in `tree -I "${ignore_files}" -Ffai --noreport`
-do
-  if [ ! -d "${input_file}" ]; then
-    echo "Processing file: ${input_file}"
-    gomplate --file "${input_file}" --out "${input_file}" --left-delim "<<[" --right-delim "]>>" -c var=customize.json
-  fi
-done
+gomplate -c var=customize.json --left-delim "<<[" --right-delim "]>>" --file ./index.html --out ./index.html
