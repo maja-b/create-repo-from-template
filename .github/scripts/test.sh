2f6969a647566fad1b92305dcb34ca22f70a5f39
maja-b <101280519+maja-b@users.noreply.github.com>
2022-11-14 14:40:26 +0100

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,1 +1,19 @@
-echo "Hello World"
+
+ignore_files=".git|.github|.gtignore|customize.json|README.md"
+
+for input_file in `tree -I "${ignore_files}" -Ffai --noreport`
+do
+  if [ ! -d "${input_file}" ]; then
+    echo "Processing file: ${input_file}"
+    gomplate \
+         -f "${input_file}" \
+         -o "${input_file}" \
+         --left-delim "<<[" \
+         --right-delim "]>>" \
+         -c var=./github/scripts/customize.json
+  fi
+done
+
+# Clean up / implode
+# rm README.md
+# rm customize
