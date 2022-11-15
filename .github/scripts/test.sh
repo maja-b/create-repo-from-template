56162d9014fedeb3e830a719c5eb47db3ede124e
maja-b <101280519+maja-b@users.noreply.github.com>
2022-11-15 16:30:23 +0100

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,1 +1,1 @@
-gomplate -c var=customize.json --left-delim "<<[" --right-delim "]>>" --file ./index.html --out ./index.html
+gomplate -c var=customize.json --left-delim "<<[" --right-delim "]>>" --file ./index.html --out ./index.html --verbose
